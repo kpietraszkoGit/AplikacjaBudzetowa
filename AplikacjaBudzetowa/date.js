7c7e775398e139f589ff7394e79f9844fbca53fd
Kamil Pietraszko <kamil.pietraszko.programista@gmail.com>
2020-02-03 04:19:05 +0100

    personalBudget

--- AplikacjaBudzetowa/date.js
+++ AplikacjaBudzetowa/date.js
@@ -7,7 +7,7 @@ function today()
 				
 		document.getElementById("days").value = currYear + "-" + ((currMonth<10) ? '0'+currMonth : currMonth )+ "-" + ((currDate<10) ? '0'+currDate : currDate );
 }
-
+  
 //przycisk menu
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
@@ -28,30 +28,3 @@ window.onclick = function(event) {
     }
   }
 }
-
-//wykres kolowy
-anychart.onDocumentReady(function() {
-
-  // set the data
-  var data = [
-      {x: "white", value: 223553265},
-      {x: "Black or African American", value: 38929319},
-      {x: "American Indian and Alaska Native", value: 2932248},
-      {x: "Asian", value: 14674252},
-      {x: "Native Hawaiian and Other Pacific Islander", value: 540013},
-      {x: "Some Other Race", value: 19107368},
-      {x: "Two or More Races", value: 9009073}
-  ];
-
-  // create the chart
-  var chart = anychart.pie();
-
-
-  // add the data
-  chart.data(data);
-
-  // display the chart in the container
-  chart.container('container');
-  chart.draw();
-
-});
