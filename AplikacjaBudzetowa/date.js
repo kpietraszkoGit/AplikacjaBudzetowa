854dd4233e6b66b0825a3175bf1d3a283d46f0d6
kpietraszkoGit <54677750+kpietraszkoGit@users.noreply.github.com>
2020-09-23 20:57:43 +0200

    Update date.js

--- AplikacjaBudzetowa/date.js
+++ AplikacjaBudzetowa/date.js
@@ -1,20 +1,13 @@
 function today()
 {
-		let d = new Date();
-		let currDate = d.getDate();
-		let currMonth = d.getMonth()+1;
-		let currYear = d.getFullYear();
-				
-		document.getElementById("days").value = currYear + "-" + ((currMonth<10) ? '0'+currMonth : currMonth )+ "-" + ((currDate<10) ? '0'+currDate : currDate );
-		document.getElementById("days2").value = currYear + "-" + ((currMonth<10) ? '0'+currMonth : currMonth )+ "-" + ((currDate<10) ? '0'+currDate : currDate );
+	let d = new Date();
+	let currDate = d.getDate();
+	let currMonth = d.getMonth()+1;
+	let currYear = d.getFullYear();
+
+	document.getElementById("days").value = currYear + "-" + ((currMonth<10) ? '0'+currMonth : currMonth )+ "-" + ((currDate<10) ? '0'+currDate : currDate );
+	document.getElementById("days2").value = currYear + "-" + ((currMonth<10) ? '0'+currMonth : currMonth )+ "-" + ((currDate<10) ? '0'+currDate : currDate );
 }
-  
-//przycisk menu
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-//function myFunction() {
- // document.getElementById("myDropdown").classList.toggle("show");
-//}
 
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
