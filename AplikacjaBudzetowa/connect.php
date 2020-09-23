74712484ef76dca0c3e0c3fdf2102470699a4bcb
kpietraszkoGit <54677750+kpietraszkoGit@users.noreply.github.com>
2020-09-23 20:56:30 +0200

    Update connect.php

--- AplikacjaBudzetowa/connect.php
+++ AplikacjaBudzetowa/connect.php
@@ -6,13 +6,4 @@
 	$db_password = "";//nie ma hasla
 	$db_name = "aplikacjabudzetowapolska";//nazwa bazy
 	
-	/*$host = "localhost";//dla localhost
-	$db_user = "kailp_budzet";//login i haslo do mySQL
-	$db_password = "Waltornia123";//nie ma hasla
-	$db_name = "kailp_aplikacjabudzetowa";//nazwa bazy*/
-	
-	/*$host = "localhost";//dla localhost
-	$db_user = "kailp_buzetpolska";//login i haslo do mySQL
-	$db_password = "Waltornia123";//nie ma hasla
-	$db_name = "kailp_aplikacjabudzetowapolska";//nazwa bazy*/
 ?>
