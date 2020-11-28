e126aa31171dc1a74ecd293553af1efe701d1a68
architektmazgaj <73141073+architektmazgaj@users.noreply.github.com>
2020-11-28 12:17:18 +0100

    new file

--- connect.php
+++
@@ -1,9 +0,0 @@
-<!--dane potrzebne do logowania do mysql-->
-<?php
-
-	$host = "localhost";//dla localhost
-	$db_user = "root";//login i haslo do mySQL
-	$db_password = "";//nie ma hasla
-	$db_name = "aplikacjabudzetowapolska";//nazwa bazy
-	
-?>
