79240c4b1f989b34499d1eeb385fb0023fae7af4
Kamil Pietraszko <54677750+kpietraszkoGit@users.noreply.github.com>
2020-11-29 07:29:09 +0100

    Update yearBalance.php
    code review

--- yearBalance.php
+++ yearBalance.php
@@ -1,5 +1,4 @@
 <?php
-
 	session_start();
 	
 	if (isset($_SESSION['ok'])) unset($_SESSION['ok']);
@@ -25,7 +24,6 @@
 <!DOCTYPE html>
 <html lang="pl">
 <head>
-
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	
@@ -47,42 +45,42 @@
 	
 	<!--wykres kolowy-->
 	<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <script type="text/javascript">
-      google.load("visualization", "1", {packages:["corechart"]});
-	  google.setOnLoadCallback(drawChart);
-      function drawChart() {
-        var data = google.visualization.arrayToDataTable([
-          ['nameCategory', 'amount'],
+	<script type="text/javascript">
+	google.load("visualization", "1", {packages:["corechart"]});
+	google.setOnLoadCallback(drawChart);
+	function drawChart() {
+	var data = google.visualization.arrayToDataTable([
+          	['nameCategory', 'amount'],
           
-          <?php 
-			while($row = mysqli_fetch_array($res))
-			{
-				echo "['".$row['nameCategory']."',".$row[ 'SUM(amount)']."],";
-			}
-          ?>
+	<?php 
+		while($row = mysqli_fetch_array($res))
+		{
+			echo "['".$row['nameCategory']."',".$row[ 'SUM(amount)']."],";
+		}
+	?>
 
         ]);
 
-        var options = {
-          title: 'WYDATKI Z BIEŻĄCEGO ROKU:',
+	var options = {
+	  title: 'WYDATKI Z BIEŻĄCEGO ROKU:',
 		  legend: {textStyle: {color: '#495057'}},
 		  titleTextStyle: {color: '#495057', fontName: 'Open Sans', fontSize: '16px'}
-        };
+	};
 
-		var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+	var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
 		chart.draw(data, options);
-	  }
+	}
 
-	  $(window).on("throttledresize", function (event) {
-		var options = {
-			width: '100%',
-			height: '100%'
-		};
+	$(window).on("throttledresize", function (event) {
+	var options = {
+		width: '100%',
+		height: '100%'
+	};
 
-		var data = google.visualization.arrayToDataTable([]);
+	var data = google.visualization.arrayToDataTable([]);
 		drawChart(data, options);
-	  });
-    </script>
+	});
+	</script>
 	<!--[if lt IE 9]>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
 	<![endif]-->
@@ -144,208 +142,209 @@
 		
 		<section>
 		
-			<div class="container register">
+		<div class="container register">
 			
                 <div class="row">
 				
                     <div class="col-lg-3 register-left">
 
-						<div id="icon"><i class="icon-money-1"></i></div>
-
+			<div id="icon"><i class="icon-money-1"></i></div>
+			    
                         <p>To nie pieniądze dają szczęście, ale to, co dzięki nim możesz zrobić ze swoim życiem. <br> – Lois P. Frankel –</p>
-                    </div>
+                    
+			</div>
 					
                     <div class="col-lg-9 register-right">
                         
 						
                         <div class="tab-content" id="myTabContent">
 						
-							<div class="row registerBalance-form justify-content-md-center">
-							
-								<div class="col-md-6 mb-4">
-								<h3 class="register-headingBalance">Bieżący rok:</h3>
-								</div>
-								
-								<div id="button" class="col-md-6 mb-4">
-			
-									<div class="dropdown">
-									  <button class="btn btn-warning dropbtn dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-										Wybierz okres
-									  </button>
-									  <div class="dropdown-menu dropdown-content" aria-labelledby="dropdownMenuButton">
-										<a class="dropdown-item" href="monthlyBalance.php">Bieżący miesiąc</a>
-										<a class="dropdown-item" href="lastMonthlyBalance.php">Poprzedni miesiąc</a>
-										<a class="dropdown-item" href="yearBalance.php">Bieżący rok</a>
-										<a class="dropdown-item" href="#" data-toggle="modal" data-target="#exampleModal">Niestandardowy</a>
-									  </div>
+				<div class="row registerBalance-form justify-content-md-center">
+
+					<div class="col-md-6 mb-4">
+					<h3 class="register-headingBalance">Bieżący rok:</h3>
+					</div>
+
+					<div id="button" class="col-md-6 mb-4">
+
+						<div class="dropdown">
+						  <button class="btn btn-warning dropbtn dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							Wybierz okres
+						  </button>
+						  <div class="dropdown-menu dropdown-content" aria-labelledby="dropdownMenuButton">
+							<a class="dropdown-item" href="monthlyBalance.php">Bieżący miesiąc</a>
+							<a class="dropdown-item" href="lastMonthlyBalance.php">Poprzedni miesiąc</a>
+							<a class="dropdown-item" href="yearBalance.php">Bieżący rok</a>
+							<a class="dropdown-item" href="#" data-toggle="modal" data-target="#exampleModal">Niestandardowy</a>
+						  </div>
+						</div>
+
+					</div>
+
+					<div class="col-md-5 balance">
+						<p class="titleBox">Przychody</p>
+
+						<table cellpadding="10" cellspacing="0">
+							<thead>
+								<tr><th>Kategorie</th><th>Przychód</th></tr>
+							</thead>
+							<tbody>
+								<?php
+								require_once "connect.php";
+								mysqli_report(MYSQLI_REPORT_STRICT);
+
+								try 
+								{
+									$connection = new mysqli($host, $db_user, $db_password, $db_name);
+									if ($connection->connect_errno!=0)
+									{
+										throw new Exception(mysqli_connect_errno());
+									}
+									else
+									{
+										$user_id = $_SESSION['id'];
+										if($result = $connection->query("SELECT user_id, SUM(amount), income_category_assigned_to_user_id, (SELECT name FROM  incomes_category_assigned_to_users WHERE incomes_category_assigned_to_users.id=incomes. income_category_assigned_to_user_id) AS nameCategory FROM incomes WHERE YEAR(date_of_income) = YEAR(CURDATE()) AND user_id='$user_id' GROUP BY  nameCategory ORDER BY SUM(amount) DESC"))
+										{
+											$sumIncomes = 0;
+											while($row = mysqli_fetch_array($result))
+											{ 
+												echo "<tr><td>".$row['nameCategory']."</td><td>".$row[ 'SUM(amount)']."</td></tr>";
+												$number = $row[ 'SUM(amount)'];
+												$sumIncomes += $number;
+											}
+										}
+										else
+										{
+											throw new Exception($connection->error);
+										}
+
+										$connection->close();
+									}
+								}
+								catch(Exception $e)
+								{
+									echo '<span style="color:red;">Błąd serwera! Przepraszamy za niedogodności!</span>';
+									echo '<br />Informacja developerska: '.$e;
+								}
+								?>
+							</tbody>
+						</table>
+					</div>
+
+					<div class="col-md-5 ml-md-1 balance">
+						<p class="titleBox">Wydatki</p>
+
+						<table cellpadding="10" cellspacing="0">
+							<thead>
+								<tr><th>Kategorie</th><th>Wydatek</th></tr>
+							</thead>
+							<tbody>
+								<?php
+								require_once "connect.php";
+								mysqli_report(MYSQLI_REPORT_STRICT);
+
+								try 
+								{
+									$connection = new mysqli($host, $db_user, $db_password, $db_name);
+									if ($connection->connect_errno!=0)
+									{
+										throw new Exception(mysqli_connect_errno());
+									}
+									else
+									{
+										$user_id = $_SESSION['id'];
+										if($result = $connection->query("SELECT user_id, SUM(amount), expense_category_assigned_to_user_id, (SELECT name FROM  expenses_category_assigned_to_users WHERE expenses_category_assigned_to_users.id=expenses. expense_category_assigned_to_user_id) AS nameCategory FROM expenses WHERE YEAR(date_of_expense) = YEAR(CURDATE()) AND user_id='$user_id' GROUP BY nameCategory ORDER BY SUM(amount) DESC"))
+										{
+											$sumExpenses = 0;
+											while($row = mysqli_fetch_array($result))
+											{ 
+												echo "<tr><td>".$row['nameCategory']."</td><td>".$row[ 'SUM(amount)']."</td></tr>";
+												$numberExpense = $row[ 'SUM(amount)'];
+												$sumExpenses += $numberExpense;
+											}
+										}
+										else
+										{
+											throw new Exception($connection->error);
+										}
+										$connection->close();
+									}
+								}
+								catch(Exception $e)
+								{
+									echo '<span style="color:red;">Błąd serwera! Przepraszamy za niedogodności!</span>';
+									echo '<br />Informacja developerska: '.$e;
+								}
+								?>
+							</tbody>
+						</table>
+					</div>
+
+					<div class="col-md-10 resumeBalance">
+						<p class="titleBox">Bilans</p>
+						<table cellpadding="10" cellspacing="0">
+							<thead>
+								<tr><th>Bilans</th><th>
+								<?php
+								$difference = $sumIncomes - $sumExpenses;
+								echo $difference;
+								?>
+								</th></tr>
+							</thead>
+						</table>
+							<?php
+								if($difference<0)
+								{	
+									 echo"<div id='sum'>Uważaj, wpadasz w długi!</div>";
+								}
+								else
+								{
+									echo"<div id='sum2'>Gratulacje. Świetnie zarządzasz finansami!</div>";
+								}
+							?>
+					</div>
+
+					<div class="col-md-10 resumeBalance">
+						<p class="titleBox">Wykres</p>
+						<div id="chart_wrap"><div id="chart_div"></div></div>
+					</div>
+
+					<!-- Modal -->
+					<form method="post" action="dateRange.php">								
+						<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+							<div class="modal-dialog" role="document">
+								<div class="modal-content">
+									<div class="modal-header">
+											<div class="icons"><h5 class="modal-title" id="exampleModalLabel"><i class='icon-calendar'></i>Wybierz zakres dat: </h5>
+											 </div>
+										<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+										  <span aria-hidden="true">&times;</span>
+										</button>
 									</div>
-								
-								</div>
-							
-								<div class="col-md-5 balance">
-									<p class="titleBox">Przychody</p>
-													
-									<table cellpadding="10" cellspacing="0">
-										<thead>
-											<tr><th>Kategorie</th><th>Przychód</th></tr>
-										</thead>
-										<tbody>
-											<?php
-													require_once "connect.php";
-													mysqli_report(MYSQLI_REPORT_STRICT);
-														
-														try 
-														{
-															$connection = new mysqli($host, $db_user, $db_password, $db_name);
-															if ($connection->connect_errno!=0)
-															{
-																throw new Exception(mysqli_connect_errno());
-															}
-															else
-															{
-																$user_id = $_SESSION['id'];
-																if($result = $connection->query("SELECT user_id, SUM(amount), income_category_assigned_to_user_id, (SELECT name FROM  incomes_category_assigned_to_users WHERE incomes_category_assigned_to_users.id=incomes. income_category_assigned_to_user_id) AS nameCategory FROM incomes WHERE YEAR(date_of_income) = YEAR(CURDATE()) AND user_id='$user_id' GROUP BY  nameCategory ORDER BY SUM(amount) DESC"))
-																{
-																	$sumIncomes = 0;
-																	while($row = mysqli_fetch_array($result))
-																	{ 
-																		echo "<tr><td>".$row['nameCategory']."</td><td>".$row[ 'SUM(amount)']."</td></tr>";
-																		$number = $row[ 'SUM(amount)'];
-																		$sumIncomes += $number;
-																	}
-																}
-																else
-																{
-																	throw new Exception($connection->error);
-																}
-															
-																$connection->close();
-															}
-														}
-														catch(Exception $e)
-														{
-															echo '<span style="color:red;">Błąd serwera! Przepraszamy za niedogodności!</span>';
-															echo '<br />Informacja developerska: '.$e;
-														}
-											?>
-										</tbody>
-									</table>
-								</div>
-								
-								<div class="col-md-5 ml-md-1 balance">
-									<p class="titleBox">Wydatki</p>
-									
-									<table cellpadding="10" cellspacing="0">
-										<thead>
-											<tr><th>Kategorie</th><th>Wydatek</th></tr>
-										</thead>
-										<tbody>
-											<?php
-													require_once "connect.php";
-													mysqli_report(MYSQLI_REPORT_STRICT);
-														
-														try 
-														{
-															$connection = new mysqli($host, $db_user, $db_password, $db_name);
-															if ($connection->connect_errno!=0)
-															{
-																throw new Exception(mysqli_connect_errno());
-															}
-															else
-															{
-																$user_id = $_SESSION['id'];
-																if($result = $connection->query("SELECT user_id, SUM(amount), expense_category_assigned_to_user_id, (SELECT name FROM  expenses_category_assigned_to_users WHERE expenses_category_assigned_to_users.id=expenses. expense_category_assigned_to_user_id) AS nameCategory FROM expenses WHERE YEAR(date_of_expense) = YEAR(CURDATE()) AND user_id='$user_id' GROUP BY nameCategory ORDER BY SUM(amount) DESC"))
-																{
-																	$sumExpenses = 0;
-																	while($row = mysqli_fetch_array($result))
-																	{ 
-																		echo "<tr><td>".$row['nameCategory']."</td><td>".$row[ 'SUM(amount)']."</td></tr>";
-																		$numberExpense = $row[ 'SUM(amount)'];
-																		$sumExpenses += $numberExpense;
-																	}
-																}
-																else
-																{
-																	throw new Exception($connection->error);
-																}
-																$connection->close();
-															}
-														}
-														catch(Exception $e)
-														{
-															echo '<span style="color:red;">Błąd serwera! Przepraszamy za niedogodności!</span>';
-															echo '<br />Informacja developerska: '.$e;
-														}
-											?>
-										</tbody>
-									</table>
-								</div>
-								
-								<div class="col-md-10 resumeBalance">
-									<p class="titleBox">Bilans</p>
-									<table cellpadding="10" cellspacing="0">
-									   <thead>
-											<tr><th>Bilans</th><th>
-											<?php
-											$difference = $sumIncomes - $sumExpenses;
-											echo $difference;
-											?>
-											</th></tr>
-										</thead>
-									</table>
-											<?php
-												if($difference<0)
-												{	
-													 echo"<div id='sum'>Uważaj, wpadasz w długi!</div>";
-												}
-												else
-												{
-													echo"<div id='sum2'>Gratulacje. Świetnie zarządzasz finansami!</div>";
-												}
-											?>
-								</div>
-								
-								<div class="col-md-10 resumeBalance">
-									<p class="titleBox">Wykres</p>
-									<div id="chart_wrap"><div id="chart_div"></div></div>
-								</div>
-								
-								<!-- Modal -->
-								<form method="post" action="dateRange.php">								
-									<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-										<div class="modal-dialog" role="document">
-											<div class="modal-content">
-												<div class="modal-header">
-														<div class="icons"><h5 class="modal-title" id="exampleModalLabel"><i class='icon-calendar'></i>Wybierz zakres dat: </h5>
-														 </div>
-													<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-													  <span aria-hidden="true">&times;</span>
-													</button>
-												</div>
-												<div class="modal-body">
-													<div class="form-group col-md-9 mx-auto">
-														<div class="to">
-															<p> od </p>
-														</div>
-														<input type="date" class="form-control" id="days" name="day1" value="" min="1900-01-01" max="2500-01-01" required />
-													</div>
-													<div class="form-group col-md-9 mx-auto">
-														<div class="to">
-															<p> do </p>
-														</div>
-														<input type="date" class="form-control" id="days2" name="day2" value="" min="1900-01-01" max="2500-01-01" required />
-													</div>
-												</div>
-												<div class="modal-footer">
-													<button type="button" class="btn btn-secondary" data-dismiss="modal">Anuluj</button>
-													<button type="submit" class="btn ok">OK</button>
-												</div>
+									<div class="modal-body">
+										<div class="form-group col-md-9 mx-auto">
+											<div class="to">
+												<p> od </p>
 											</div>
+											<input type="date" class="form-control" id="days" name="day1" value="" min="1900-01-01" max="2500-01-01" required />
+										</div>
+										<div class="form-group col-md-9 mx-auto">
+											<div class="to">
+												<p> do </p>
+											</div>
+											<input type="date" class="form-control" id="days2" name="day2" value="" min="1900-01-01" max="2500-01-01" required />
 										</div>
 									</div>
-								</form>
-
+									<div class="modal-footer">
+										<button type="button" class="btn btn-secondary" data-dismiss="modal">Anuluj</button>
+										<button type="submit" class="btn ok">OK</button>
+									</div>
+								</div>
 							</div>
+						</div>
+					</form>
+
+				</div>
 							
                         </div>
 						
@@ -353,7 +352,7 @@
 					
                 </div>
 
-            </div>
+            	</div>
 				
 		</section>
 		
